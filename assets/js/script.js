abcf7affc2eec1913f46c081e810293f69a0d8af
Philip Tantsidis <ptantsidis@gmail.com>
2021-09-27 14:44:52 -0400

    first commit Phil

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+let searchBtn = document.querySelector("button");
+
+// $( function() {
+//     let availableFormats = [
+//       "maps",
+//       "audio",
+//       "photos",
+//       "manuscripts",
+//     ];
+//     $( function() {
+//         $(availableFormats).selectmenu();
+    
+//   } );
+function getApi() {
+console.log("get api button clicked")
+
+}
+
+
+searchBtn.addEventListener('click', getApi);
